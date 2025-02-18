3d5e66e54a2c24ca3c1d73d456dc4e864ef3426e
syeda <syedafarah776@gmail.com>
2025-02-19 01:07:27 +0600

    theme change added

--- script.js
+++ script.js
@@ -6,3 +6,36 @@ navToggle.addEventListener('click', () => {
     navToggle.classList.toggle('animate-toggle')
 
 })
+
+
+// =================style-switcher====================
+const styleSwitcher = document.getElementById('style-switcher')
+switcherToggle = document.getElementById('switcher-toggle')
+switcherClose = document.getElementById('switcher-close')
+// switcher-show===================
+switcherToggle.addEventListener('click', () => {
+    styleSwitcher.classList.add('show-switcher')
+})
+switcherClose.addEventListener('click', () => {
+    styleSwitcher.classList.remove('show-switcher')
+})
+// theme colors============================
+const colors = document.querySelectorAll('.style-switcher-color')
+colors.forEach((color)=> {
+    color.onclick = () =>{
+        const activeColor = color.style.getPropertyValue('--hue');
+        colors.forEach((c) => c.classList.remove('active-color'));
+        color.classList.add('active-color');
+        document.documentElement.style.setProperty('--hue', activeColor);
+    }
+})
+
+// light dark mood==============
+ let currentTheme = 'light';
+ document.body.className =  currentTheme;
+
+ document.querySelectorAll('input[name="body-theme"]').forEach((input) =>{ input.addEventListener('change', () =>{
+    currentTheme= input.value;
+     document.body.className = currentTheme;
+ })
+})
